035ed1d4756da919090d43079f41f8b6f16912fe
Saurav Saini <killersaurav13@gmail.com>
2026-04-19 00:59:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,72 +11,41 @@ div.innerText = text;
 terminal.appendChild(div);
 }
 
-  
-function loading(text = "Scanning", duration = 1000) {
-return new Promise((resolve) => {
-const div = document.createElement("div");
-terminal.appendChild(div);
-
-let dots = 0;
-
-const interval = setInterval(() => {
-  div.innerText = text + ".".repeat(dots % 4);
-  dots++;
-}, 300);
-
-setTimeout(() => {
-  clearInterval(interval);
-  div.innerText = text + "... done";
-  resolve();
-}, duration);
-
-});
-}
-  
-async function runCommand(cmd) {
+function runCommand(cmd) {
 print("root@0xsaurav-exe:~$ " + cmd);
 
 if (cmd === "whoami") {
-print("0xsaurav-exe");
+  print("0xsaurav-exe");
 
 } else if (cmd === "about") {
-print("Cybersecurity Trainee focused on VAPT and penetration testing.");
+  print("Cybersecurity Trainee focused on VAPT and penetration testing.");
 
 } else if (cmd === "projects") {
-loading("[+] Scanning projects", 1200);
-
-print(" ");
-print("=== Projects ===");
-
-print("• Network Enumeration Lab");
-print("• Linux Privilege Escalation");
-print("• Web Application Security Testing");
-
-print(" ");
+  print("Projects:");
+  print("- Network Enumeration Lab");
+  print("- Linux Privilege Escalation");
+  print("- Web Application Security Testing");
 
 } else if (cmd === "contact") {
-loading("[+] Fetching contact", 1000);
+  print("Contact:");
+  print("LinkedIn: linkedin.com/in/saurav-saini-eh");
+  print("TryHackMe: tryhackme.com/p/KillerSourav");
+  print("GitHub: github.com/0xsaurav-exe");
+  print("Email: sauravsaini31609@gmail.com");
 
-print(" ");
-print("=== Contact ===");
-
-print("LinkedIn: linkedin.com/in/saurav-saini-eh");
-print("TryHackMe: tryhackme.com/p/KillerSourav");
-print("GitHub: github.com/0xsaurav-exe");
-print("Email: sauravsaini31609@gmail.com");
-
-print(" ");
-  
 } else if (cmd === "clear") {
-const header = document.getElementById("terminalHeader");
-terminal.innerHTML = "";
-terminal.appendChild(header);
-return;
+  const header = document.getElementById("terminalHeader");
+  terminal.innerHTML = "";
+  terminal.appendChild(header);
+  return;
 
 } else {
-print("Command not found");
+  print("Command not found");
 }
-}form.addEventListener("submit", function (e) {
+
+}
+
+form.addEventListener("submit", function (e) {
 e.preventDefault();
 
 const cmd = input.value.trim();
